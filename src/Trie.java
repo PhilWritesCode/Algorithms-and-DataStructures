612f2e4b3ce3301c1db0f723664bf55464d85a70
Phil Jenkins <philip.t.jenkins@gmail.com>
2015-08-13 00:13:03 -0700

    added prefix stuff

--- src/Trie.java
+++ src/Trie.java
@@ -54,7 +54,7 @@ public class Trie {
         return false;
     }
 
-    public int get(String key) {
+    public Node get(String key) {
         Node node = root;
         int keyIndex = 0;
 
@@ -65,9 +65,33 @@ public class Trie {
             nextKey = key.charAt(keyIndex++);
         }
 
-        if(keyIndex == key.length()) return node.value;
+        if(keyIndex == key.length()) return node;
 
-        return -1;
+        return null;
+    }
+
+    public Iterable<String> startsWith(String prefix) {
+        ArrayQueue<String> queue = new ArrayQueue();
+
+        Node prefixNode = get(prefix);
+        StringBuilder builder = new StringBuilder(prefix);
+
+        gatherWords(builder, queue, prefixNode);
+
+        return queue;
+    }
+
+    private void gatherWords(StringBuilder builder, ArrayQueue<String> queue, Node prefixNode) {
+
+        if(prefixNode.value != -1) queue.enqueue(builder.toString());
+
+        for(char c = 0; c < 256; c++) {
+            if(prefixNode.children[c] != null) {
+                builder.append(c);
+                gatherWords(builder, queue, prefixNode.children[c]);
+                builder.deleteCharAt(builder.length()-1);
+            }
+        }
     }
 
     public static void main(String[] args) {
@@ -76,12 +100,21 @@ public class Trie {
         System.out.println("Contains Anna? " + t.contains("Anna"));
         t.insert("Anna", 26);
         t.insert("Anne", 24);
+        t.insert("Apple", 40);
+        t.insert("Anagram", 91);
+        t.insert("Anagranimals", 12);
+        t.insert("George", 19);
+        t.insert("Geoff", 32);
         System.out.println("Contains Anna? " + t.contains("Anna"));
-        System.out.println("Anna: " + t.get("Anna"));
+        System.out.println("Anna: " + t.get("Anna").value);
         System.out.println("Contains Anne? " + t.contains("Anne"));
-        System.out.println("Anne: " + t.get("Anne"));
+        System.out.println("Anne: " + t.get("Anne").value);
         t.insert("Anne", 31);
-        System.out.println("Anne: " + t.get("Anne"));
+        System.out.println("Anne: " + t.get("Anne").value);
         System.out.println("Contains Ann? " + t.contains("Ann"));
+
+        for(String word : t.startsWith("An")) {
+            System.out.println("Starts with 'An': " + word);
+        }
     }
 }
